c2cba053305799a2f683441de2ec640c1455db8e
Victor De Menezes <estudiodemenezes@gmail.com>
2026-06-20 18:47:10 +0200

    chore: eslint config and scafolding style
    Eslint rules and react compiler plugin, also scafolding for global css

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,12 +6,30 @@ const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
+  {
+    plugins: {
+      "react-compiler": reactCompiler,
+    },
+    rules: {
+      "react-compiler/react-compiler": "error",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "node_modules/**",
   ]),
 ]);
 
