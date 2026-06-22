b8bd1ed03b604ffd7be9ebcd2c5bda8c6b37ef92
Victor De Menezes <estudiodemenezes@gmail.com>
2026-06-22 14:57:14 +0200

    feat: Checkout feature components and hook
    Add useDebounce, Cart Context - Provider, CartButton, Footer, CartItem, useCart, and styles

--- src/features/checkout/context/CartProvider.tsx
+++ src/features/checkout/context/CartProvider.tsx
@@ -13,7 +13,6 @@ export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
   "use memo";
   const [cart, setCart] = useState<CartItem[]>([]);
 
-  // 1. Persistencia: Cargar el carrito desde LocalStorage al iniciar (Solo en el cliente)
   useEffect(() => {
     const storedCart = localStorage.getItem("zara_cart");
     if (storedCart) {
@@ -26,7 +25,6 @@ export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
     }
   }, []);
 
-  // 2. Persistencia: Guardar en LocalStorage cada vez que cambie el carrito
   useEffect(() => {
     if (cart.length > 0) {
       localStorage.setItem("zara_cart", JSON.stringify(cart));
@@ -35,10 +33,8 @@ export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
     }
   }, [cart]);
 
-  // 3. Funciones de negocio mutadoras
   const addToCart = (newItem: CartItem) => {
     setCart((prevCart) => {
-      // Validamos si ya existe el producto con las MISMAS variantes
       const existingItemIndex = prevCart.findIndex(
         (item) =>
           item.product.id === newItem.product.id &&
@@ -75,8 +71,6 @@ export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
 
   const clearCart = () => setCart([]);
 
-  // 4. Cálculos derivados (Totales)
-  // Usamos useMemo para optimizar y evitar recalcular en renders irrelevantes
   const totals = useMemo(() => {
     return cart.reduce(
       (acc, item) => {
@@ -90,7 +84,6 @@ export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
     );
   }, [cart]);
 
-  // 5. El valor del contexto expuesto
   const contextValue = {
     cart,
     addToCart,
